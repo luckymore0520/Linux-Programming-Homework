850f7fb28e2e29db85a5399683426c88889e9dc9
wk121250151 <kswangk@163.com>
2015-04-07 19:44:19 +0800

    submit homework

--- main.c
+++ main.c
@@ -3,15 +3,11 @@
 //  CTest
 //
 //  Created by luck-mac on 15/4/5.
-//  Copyright (c) 2015年 com.njutec. All rights reserved.
+//  Copyright (c) 2015年 com.nju. All rights reserved.
 //
 
-
-
-
-
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 #define INPUT 0
 #define OUTPUT 1
@@ -22,7 +18,6 @@ int main(int argc, const char * argv[]) {
     char buf[BUFFER_LEN];
     int return_count;//返回编号
     memset(buf, '\0', sizeof(buf));
-    FILE *pipeFile = popen("ls", "r");
     pipe(file_descriptors);
     if ((pid = fork())==-1) {
         printf("Fork 发生错误");
@@ -30,17 +25,17 @@ int main(int argc, const char * argv[]) {
     }
     //子进程
     if (pid == 0) {
+        FILE *pipeFile = popen("ls", "r");
         close(file_descriptors[INPUT]);
         fread(buf, sizeof(char), sizeof(buf), pipeFile);
-        printf("内容:%s",buf);
         write(file_descriptors[OUTPUT], buf,strlen(buf));
         return 0;
     } else {
         //父进程
-        wait(NULL);
+        wait(NULL);//阻塞，在子线程执行完后再执行父进程
         close(file_descriptors[OUTPUT]);
         return_count = read(file_descriptors[INPUT], buf, sizeof(buf));
-        printf("%d bytes 数据被读到了: %s\n",return_count, buf);
+        printf("%s", buf);
     }
     return 0;
 }
